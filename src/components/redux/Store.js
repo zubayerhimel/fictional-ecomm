d714368e34573b39333c6099f54d6da747099560
zubayerhimel <zhimel007@gmail.com>
2020-08-15 15:25:19 +0600

    implemented redux-persist for local storage

--- src/components/redux/Store.js
+++ src/components/redux/Store.js
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import rootReducer from "./RootReducer";
 
 const middleWares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const store = createStore(rootReducer, applyMiddleware(...middleWares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "../src/components/redux/Store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../src/components/redux/Store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
